d3d3f7ff87bb3f2926a2668d8be1351d5049d0af
maancrnj <cornejomarkangelo00180@gmail.com>
2022-06-10 21:38:06 +0800

    Initial commit for project creation

---
+++ CDMLibSys/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.practice.cdmlibsys {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.practice.cdmlibsys to javafx.fxml;
+    exports com.mycompany.practice.cdmlibsys;
+}
+*/
